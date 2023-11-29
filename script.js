e6465fc874a4974252eb4056a032463baeb2ae73
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-11-29 20:17:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let buttons = document.getElementById("btn");
+
+buttons.addEventListener("click", (event)  =>{
+	event.preventDefault();
+	let nameinput = document.getElementById("name").value;
+	let ageinput = document.getElementById("age").value;
+
+	return new Promise((resolve, reject) => {
+		setTimeout(()=>{
+			if(age > 18){
+				resolve(
+					alert(`Welcome, ${nameinput}. you can vote`)
+				)
+				
+			}
+			else {
+				reject(
+					alert(`oh sorry ${nameinput}. You aren't old enough`)
+				)
+			}
+		},4000)
+	})
+})
